98ba60b990be3bbbd8ba062c032eaa61a699c07f
unknown <thraverdy@gmail.com>
2020-12-02 22:35:19 +0100

    new deploy to heroku cli

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,12 @@ while true; do
   read -p "You sure want to deploy (develop -> main) ? [y/N]: " yn
   case $yn in
   [Yy]*)
-    git push origin develop:main
+    git stash
+    git checkout main
+    git push heroku main
+    git checkout -
+    git stash pop
+    # git push origin develop:main
     break
     ;;
   *)
