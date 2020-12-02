cc57623d8fc8dbf7e0ec344dc6117ed2171526b2
unknown <thraverdy@gmail.com>
2020-12-02 20:56:18 +0100

    Express init !

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 3000);
+
+// route our app
+app.get('/', function (_, res) {
+	res.send('hello world!');
+});
+
+app.post('/', function (req, res) {
+	res.send(req.body);
+});
