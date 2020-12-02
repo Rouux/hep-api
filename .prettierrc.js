26ec6502d68034f551692553e485f078d4866eea
unknown <thraverdy@gmail.com>
2020-12-02 21:05:20 +0100

    prettier !

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+// http://json.schemastore.org/prettierrc
+
+module.exports = {
+	tabWidth: 2,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	arrowParens: 'avoid',
+	endOfLine: 'crlf',
+	printWidth: 80,
+	proseWrap: 'never'
+};
