856f6aef2ee181b36d9920e8fa06b34c3d07261c
unknown <thraverdy@gmail.com>
2020-12-02 22:42:34 +0100

    deplooooy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,7 @@ while true; do
   read -p "You sure want to deploy (develop -> main) ? [y/N]: " yn
   case $yn in
   [Yy]*)
+    git rebase develop develop
     git checkout main
     git rebase
     git merge develop
