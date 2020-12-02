fa5dcd84cc4b2374ab3e996a982f001a14f5a12a
unknown <thraverdy@gmail.com>
2020-12-02 21:45:19 +0100

    typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  read -p "You sure want to deploy (develop -> master) ? [y/N]: " yn
+  read -p "You sure want to deploy (develop -> main) ? [y/N]: " yn
   case $yn in
   [Yy]*)
     git push origin develop:main
